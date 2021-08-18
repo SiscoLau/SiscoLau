d1b6aff6a9f6fda728fde266f58a6f37ed1742ff
SiscoLau <71496835+SiscoLau@users.noreply.github.com>
2021-08-18 18:00:03 +0800

    Delete ALL4POP directory

--- ALL4POP/Popass_Pig.js
+++
@@ -1,7 +0,0 @@
-/*
-  Simple 800 clicks/10 second bot for poppig.click
-  Please avoid clicking on the pig while script is running to avoid clicks rejected by server
-*/
-
-setInterval(function(){ clicked = 800; }, 5000);
-
