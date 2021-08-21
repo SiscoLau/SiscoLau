a6325aa6eb59ed72f2389eabad8f68fc386d5559
SiscoLau <71496835+SiscoLau@users.noreply.github.com>
2021-08-21 17:17:07 +0800

    Create PopDog.9.8_2K.js

---
+++ ALL4POP/PopDog.9.8_2K.js
@@ -0,0 +1,35 @@
+const interval = 9_800;     //3.8 seconds per request
+const clicksToSend = 2000;  //2000 is the maximum amount of clicks can be incremented per request, no point going higher
+
+console.clear();
+console.log(`%cBot will send ${clicksToSend} clicks every ${interval / 1000.0} seconds.`, "color: #ff77ff");
+console.log(`%cYour UUID is ${uuid}.`, "color: #ff77ff");
+
+const requester = async () => {
+    try {
+        const res = await fetch('/clicked/v2', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify({
+                clicks: clicksToSend,
+                uuid: uuid,
+                username: username,
+            }),
+        });
+        const data = await res.json();
+        myClicks = data.clicks;
+        console.log(`%cTotal clicks accumulated: ${myClicks}.`, "color: #6ea561")
+        prevClicks = myClicks;
+        prevUploadedClicks = myClicks;
+    }
+    catch (err)
+    {
+        console.log(err);
+        console.log('%cWaiting for next run.', "color: #fbb40c");
+    }
+}
+
+setInterval(requester, interval);
+requester();
