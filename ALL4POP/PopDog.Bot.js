6d60bd6e074b49318b8c60104710e70ae56f96a8
SiscoLau <71496835+SiscoLau@users.noreply.github.com>
2021-08-21 17:04:12 +0800

    Update PopDog.Bot.js

--- ALL4POP/PopDog.Bot.js
+++ ALL4POP/PopDog.Bot.js
@@ -1,37 +1,14 @@
-const interval = 3_800;     //3.8 seconds per request
-const clicksToSend = 2000;  //2000 is the maximum amount of clicks can be incremented per request, no point going higher
-
-console.clear();
-console.log(`%cBot will send ${clicksToSend} clicks every ${interval / 1000.0} seconds.`, "color: #ff77ff");
-console.log(`%cYour UUID is ${uuid}.`, "color: #ff77ff");
-
-const requester = async () => {
-    try {
-        const res = await fetch('/clicked/v2', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8'
-            },
-            body: JSON.stringify({
-                clicks: clicksToSend,
-                uuid: uuid,
-                username: username,
-            }),
-        });
-        const data = await res.json();
-        myClicks = data.clicks;
-        console.log(`%cTotal clicks accumulated: ${myClicks}.`, "color: #6ea561")
-        prevClicks = myClicks;
-        prevUploadedClicks = myClicks;
-    }
-    catch (err)
-    {
-        console.log(err);
-        console.log('%cWaiting for next run.', "color: #fbb40c");
-    }
-}
-
-setInterval(requester, interval);
-requester();
-
-
+if (window.location.href !== "https://popdog.click/play") alert("click play or goto https://popdog.click/play");
+setInterval(() => {
+    fetch("/clicked/v2", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json;charset=utf-8"
+        },
+        body: JSON.stringify({
+            clicks: 2000,
+            uuid: window.localStorage.uuid,
+            username: "SaVe_HK"
+        })
+    })
+}, 5000)
